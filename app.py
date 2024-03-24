32fad7a4084f4c0ee59b7265f96e7d9d3e7fa8aa
Andamlak Abza <terbu102@gmail.com>
2024-03-24 13:32:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 @app.errorhandler(404)
-def errorhj(error):
+def not_found(error):
     return render_template('404.html')
 
 
